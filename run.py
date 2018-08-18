40ecd548221df74791cbdaf9287843b2fc6fb42d
Yong Hao TENG <yonghao.teng@gmail.com>
2018-08-18 21:28:21 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,14 +4,39 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 def notify(mesg):
-    print(mesg)
-    os.system('telegram-send --format markdown --config home-group.conf \"' + mesg + '\"')
+    print(mesg.replace('\\n', '\n'))
+    os.system('printf \"{}\" | telegram-send --format markdown --config telegram.conf --stdin'.format(mesg))
+
+def print_expiring_items(items, expiring_in_days):
+    print_items(items, 'These items are expiring in the next `{}` days\\n'.format(expiring_in_days))
+
+def print_deleted_items(items):
+    print_items(items, "Removed these items from gsheet...\\n")
+
+def print_error_items(items):
+    print_items(items, "Warning these items does not have date or name\\n", False)
+
+def print_items(items, header_mesg, sort=True):
+    if len(items) > 0:
+        if sort:
+            items.sort(key=lambda r: datetime.datetime.strptime(r['DATE'], "%d/%m/%Y"))
+
+        mesg = header_mesg
+        mesg += '\\n'
+
+        for item in items:
+            mesg += '{} | {}\\n'.format(item['DATE'], item['NAME'])
+        
+        notify(mesg)
+
+def convert_to_datetime(str):
+    return datetime.datetime.strptime(str, "%d/%m/%Y")
 
 def check():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
     client = gspread.authorize(creds)
+
     sheet = client.open("item expiry list").sheet1
     records = sheet.get_all_records()
 
@@ -21,44 +46,40 @@ def check():
 
     items_expiring_in_a_month = []
     items_expiring_in_a_week = []
-    items_removed = []
+    items_deleted = []
+    items_without_date_or_name = []
+
+    rows_to_delete = []
 
     for row, record in enumerate(records):
-        item_date = datetime.datetime.strptime(record['DATE'], "%d/%m/%Y").date()
-
-        if item_date <= one_month_from_today and record['NOTIFIED'] is not 'T':
+        if not record['DATE'] or not record['NAME']:
+            items_without_date_or_name.append(record)
+            continue
+        
+        item_date = convert_to_datetime(record['DATE']).date()
+        
+        if item_date <= one_month_from_today and record['NOTIFIED'] == '':
             items_expiring_in_a_month.append(record)
             # Sheet counts from 1 
             # Add additional 1 to skip header row
             sheet.update_cell(row + 2, 3, 'T')
         
-        elif item_date <= one_week_from_today and record['NOTIFIED'] is 'T':
+        elif item_date <= one_week_from_today and record['NOTIFIED'] == 'T':
             items_expiring_in_a_week.append(record)
             sheet.update_cell(row + 2, 3, 'TT')
         
-        elif record['NOTIFIED'] is 'TT':
-            items_removed.append(record)
-            sheet.delete_row(row + 2)
+        elif record['NOTIFIED'] == 'TT':
+            items_deleted.append(record)
+            rows_to_delete.append(row)
 
-    if len(items_expiring_in_a_month) > 0:
-        mesg = 'Hey! These items are expiring in the next \`31\` days\n'
-        
-        for i, item in enumerate(items_expiring_in_a_month):
-            if i % 2 == 0: mesg += '\`\`\`'
-            mesg += '{} | {}\n'.format(item['DATE'], item['NAME'])
-            if i % 2 == 0: mesg += '\`\`\`'
+    # Delete rows last and from larger row number to small row number to peserve correct row states
+    for row in reversed(rows_to_delete):
+        sheet.delete_row(row + 2)
 
-        notify(mesg)
-
-    if len(one_week_from_today) > 0:
-        mesg = 'These items are expiring in the next \`7\` days\n'
-        
-        for i, item in enumerate(one_week_from_today):
-            if i % 2 == 0: mesg += '\`\`\`'
-            mesg += '{} | {}\n'.format(item['DATE'], item['NAME'])
-            if i % 2 == 0: mesg += '\`\`\`'
-
-        notify(mesg)
+    print_expiring_items(items_expiring_in_a_month, 31)
+    print_expiring_items(items_expiring_in_a_week, 7)
+    print_deleted_items(items_deleted)
+    print_error_items(items_without_date_or_name)
 
 if __name__ == '__main__':
     check()
